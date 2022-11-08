30956934b5b6139b59ced2902a2eb6c29af26900
ktp0li <polishka3004@gmail.com>
2022-11-08 16:23:44 -0500

    init commit

---
+++ lab/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    opennebula = {
+      source  = "OpenNebula/opennebula"
+      version = "1.0.1"
+    }
+  }
+}
+
+provider "opennebula" {
+  endpoint = "http://localhost:2633/RPC2"
+  username = "oneadmin"
+  password = "qwe"
+  insecure = true
+}
+
+variable "user-id" {
+  type = string
+  default = "test"
+}
+
+data "opennebula_virtual_network" "nat" {
+  name = "NAT"
+}
+
+data "opennebula_template" "deb" {
+  name = "test"
+  has_cpu = true
+  has_vcpu = true
+  has_memory = true
+}
+
+resource "opennebula_virtual_machine" "r1" {
+  name        = format("%s-%s",var.user-id,"lab1-r1")
+  template_id = data.opennebula_template.deb.id
+  nic {
+    network_id = data.opennebula_virtual_network.nat.id
+  }
+  nic {
+    
+}
+}
+
