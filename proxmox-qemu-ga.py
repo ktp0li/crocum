595c66573b01b9e6ce073436b7166184a588a51d
poli <polishka3004@gmail.com>
2022-11-27 21:23:05 +0300

    added execute_status()

--- proxmox-qemu-ga.py
+++ proxmox-qemu-ga.py
@@ -25,19 +25,34 @@ def open_file(filepath):
 
 
 def write_file(handle, buf_file):
-    command = f"{{'execute':'guest-file-write',\
-'arguments':{{'handle':{handle},'buf-b64':{buf_file}}}}}"
+    command = f"{{'execute':'guest-file-write','arguments':\
+{{'handle':{handle},'buf-b64':{buf_file}}}}}"
     client.send(command.encode())
     print(command)
     print(client.recv(2048).decode())
 
 
 def close_file(handle):
-    command = f"{{'execute':'guest-file-close',\
-'arguments':{{'handle':{handle}}}}}"
+    command = f"{{'execute':'guest-file-close','arguments':\
+{{'handle':{handle}}}}}"
     client.send(command.encode())
     print(command)
     print(client.recv(2048).decode())
+
+
+def execute(remote_command, flags):
+    command = f"{{'execute':'guest-exec','arguments':\
+{{'path':'{remote_command}','arg':{flags},'capture-output':true}}}}"
+    client.send(command.encode())
+    answer = client.recv(2048).decode()
+    return json.loads(answer)['return']['pid']
+
+
+def execute_status(pid):
+    command = f"{{'execute':'guest-exec-status','arguments':{{'pid':{pid}}}}}"
+    client.send(command.encode())
+    answer = json.loads(client.recv(2048).decode())['return']
+    return answer['exitcode'], answer['out-data']
 
 
 if args.file:
@@ -47,6 +62,11 @@ if args.file:
     close_file(handle)
 
 if args.execute:
-    command = args.execute.split()[0]
-    flags = args.execute.split()[1:]
-    print(command, flags)
+    flags = []
+    command = args.execute
+    if len(command.split()) > 1:
+        flags = command.split()[1:]
+        command = command.split()[0]
+    exitcode, outdata = execute_status(execute(command, flags))
+    print('exitcode:', exitcode)
+    print(base64.b64decode(outdata).decode().replace('\n', ''))
