8980477ebe8e32ec969bcb8f30903fe4f12f1033
poli <polishka3004@gmail.com>
2022-11-27 14:51:31 +0300

    finished open_file()

--- proxmox-qemu-ga.py
+++ proxmox-qemu-ga.py
@@ -1,8 +1,9 @@
+import os
 import socket
 import base64
+import json
 client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 client.connect("/var/run/qemu-server/101.qga")
-handler = 0
 test = "/tmp/test.py"
 
 
@@ -10,7 +11,8 @@ def open_file(file):
     command = f"{{'execute':'guest-file-open','arguments':\
 {{'path':'{file}','mode':'w+'}}}}"
     client.send(command.encode())
-    print(client.recv(2048).decode())
+    answer = client.recv(2048).decode()
+    return json.loads(answer)['return']
 
 
-open_file(test)
+handler = open_file(test)
