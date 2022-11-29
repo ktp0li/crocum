5274b89826d1e08bcb74a6660f5ebeaf98ce609c
poli <polishka3004@gmail.com>
2022-11-29 21:17:14 +0300

    refactored code

--- scripts/proxmox-qemu-ga.py
+++ scripts/proxmox-qemu-ga.py
@@ -12,8 +12,8 @@ def connect_to_socket(vm_id):
 
 
 def open_file(client, filepath):
-    command = f"{{'execute':'guest-file-open','arguments':\
-{{'path':'{filepath}','mode':'w+'}}}}"
+    command = json.dumps({'execute': 'guest-file-open', 'arguments':
+                          {'path': filepath, 'mode': 'w+'}})
     client.send(command.encode())
     answer = client.recv(2048).decode()
     print(answer)
@@ -21,30 +21,32 @@ def open_file(client, filepath):
 
 
 def write_file(client, handle, buf_file):
-    command = f"{{'execute':'guest-file-write','arguments':\
-{{'handle':{handle},'buf-b64':{buf_file}}}}}"
+    command = json.dumps({'execute': 'guest-file-write', 'arguments':
+                          {'handle': handle, 'buf-b64': buf_file}})
     client.send(command.encode())
     print(client.recv(2048).decode())
 
 
 def close_file(client, handle):
-    command = f"{{'execute':'guest-file-close','arguments':\
-{{'handle':{handle}}}}}"
+    command = json.dumps({'execute': 'guest-file-close', 'arguments':
+                          {'handle': handle}})
     client.send(command.encode())
     print(command)
     print(client.recv(2048).decode())
 
 
 def execute(client, remote_command, flags):
-    command = f"{{'execute':'guest-exec','arguments':\
-{{'path':'{remote_command}','arg':{flags},'capture-output':true}}}}"
+    command = json.dumps({'execute': 'guest-exec', 'arguments':
+                          {'path': remote_command, 'arg': flags,
+                           'capture-output': True}})
     client.send(command.encode())
     answer = client.recv(2048).decode()
     return json.loads(answer)['return']['pid']
 
 
 def execute_status(client, pid):
-    command = f"{{'execute':'guest-exec-status','arguments':{{'pid':{pid}}}}}"
+    command = json.dumps({'execute': 'guest-exec-status', 'arguments':
+                          {'pid': pid}})
     client.send(command.encode())
     answer = json.loads(client.recv(2048).decode())['return']
     return answer['exitcode'], answer['out-data']
@@ -54,13 +56,13 @@ def qemu_write_file(args):
     if args.host_file[0] == '/':
         host_file = args.host_file
     else:
-        host_file = f'{os.path.dirname(os.path.realpath(__file__))}\
+        host_file = f'{os.getcwd()}\
 /{args.host_file}'
     file = base64.b64encode(bytes(open(host_file).read(), 'utf-8'))
     client = connect_to_socket(args.vm_id)
     path = args.remote_file
     handle = open_file(client, path)
-    write_file(client, handle, str(file)[1:])
+    write_file(client, handle, str(file)[2:-1])
     close_file(client, handle)
 
 
@@ -82,12 +84,12 @@ def parse_args():
     subparsers = parser.add_subparsers()
     parser_exec = subparsers.add_parser('exec')
     parser_file = subparsers.add_parser('file')
+    parser_file.add_argument('host_file', type=str, help='host file path')
     parser_file.add_argument('remote_file', type=str, help='remote file path')
-    parser_file.add_argument('host_file')
     parser_exec.add_argument('command', type=str, help='execute command')
     parser_exec.set_defaults(func=qemu_execute_command)
     parser_file.set_defaults(func=qemu_write_file)
-    parser.add_argument('vm_id')
+    parser.add_argument('vm_id', type=int)
     return parser.parse_args()
 
 
