37798daa7dec118f60e0457ea9560f248d3485fc
poli <polishka3004@gmail.com>
2023-01-22 02:31:26 +0300

    api: init support of SQLAlchemy and code refactoring

--- api.py
+++ api.py
@@ -3,63 +3,91 @@ from flask import Flask
 from flask.views import MethodView
 from libterraform import TerraformCommand
 from threading import Thread
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+db = SQLAlchemy(app)
+dir = '/home/user/lab/terraform'
 
 
-class UserAPI(MethodView):
-    def __init__(self):
-        self.dir = '/home/user/lab/terraform'
-        self.cli = TerraformCommand(self.dir)
-
-    # list users or lab status
-    def get(self, user_id):
-        if user_id is None:
-            stdout = self.cli.workspace('list').value
-            return stdout.split('\n')
-        else:
-            self.cli.workspace('select', str(user_id))
-            return self.cli.state('list').value
-
-    # def for ferraform apply/destroy
-    def change_lab(self, action, userid):
-        self.cli.workspace('select', userid)
-        option = {'var': f'user-id={userid}', 'auto-approve': ...}
-        self.cli.run(action, option)
-        self.cli.workspace('select', 'default')
-
-    # deploy lab
-    def post(self):
-        user_id = str(1)
-        if self.cli.workspace('new', user_id).retcode in [0, 2]:
-            self.thread = Thread(target=self.change_lab,
-                                 args=('apply', user_id), daemon=True)
-            self.thread.start()
-            return 'Lab is creating...', 202
-        else:
-            return 'Lab already exist', 409
-
-    # destroy lab
-    def delete(self, user_id):
-        if self.cli.workspace('select', str(user_id)).retcode in [0, 2]:
-            self.thread = Thread(target=self.change_lab,
-                                 args=('destroy', str(user_id)), daemon=True)
-            self.thread.start()
-            return 'Lab is destroying...', 202
-        else:
-            return 'Lab is not exists', 409
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
 
 
-user_view = UserAPI.as_view('user_api')
-app.add_url_rule('/api/lab1', defaults={'user_id': None},
-                 view_func=user_view, methods=['GET', ])
-app.add_url_rule('/api/lab1', view_func=user_view, methods=['POST', ])
-app.add_url_rule('/api/lab1/<int:user_id>', view_func=user_view,
-                 methods=['GET', 'DELETE', ])
+class Lab1(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    state = db.Column(db.String)
+
+
+def api_creation(lab_name):
+    class UserAPI(MethodView):
+        def __init__(self):
+            self.dir = f'{dir}/{lab_name}'
+            self.cli = TerraformCommand(self.dir)
+
+        # list users or lab status
+        def get(self, user_id):
+            if user_id is None:
+                stdout = self.cli.workspace('list').value
+                return stdout.split('\n')
+            else:
+                self.cli.workspace('select', str(user_id))
+                return self.cli.state('list').value
+
+        # def for ferraform apply/destroy
+        def change_lab(self, action, userid):
+            self.cli.workspace('select', userid)
+            option = {'var': f'user-id={userid}', 'auto-approve': ...}
+            self.cli.run(action, option)
+            self.cli.workspace('select', 'default')
+
+        # deploy lab
+        def post(self):
+            user_id = str(1)
+            if self.cli.workspace('new', user_id).retcode in (0, 2):
+                self.thread = Thread(target=self.change_lab,
+                                     args=('apply', user_id), daemon=True)
+                self.thread.start()
+                return {'status': 202,
+                        'message': f'{lab_name} is creating...'}, 202
+            else:
+                return {'status': 409,
+                        'message': f'{lab_name} already exist'}, 409
+
+        # destroy lab
+        def delete(self, user_id):
+            if self.cli.workspace('select', str(user_id)).retcode in [0, 2]:
+                self.thread = Thread(
+                        target=self.change_lab,
+                        args=('destroy', str(user_id)), daemon=True)
+                self.thread.start()
+                return {'status': 202,
+                        'message': f'{lab_name} is destroying...'}, 202
+            else:
+                return {'status': 409,
+                        'message': f'{lab_name} is not exists'}, 409
+
+    user_view = UserAPI.as_view('user_api')
+    app.add_url_rule(f'/api/{lab_name}', defaults={'user_id': None},
+                     view_func=user_view, methods=['GET', ])
+    app.add_url_rule(f'/api/{lab_name}', view_func=user_view,
+                     methods=['POST', ])
+    app.add_url_rule(f'/api/{lab_name}/<int:user_id>', view_func=user_view,
+                     methods=['GET', 'DELETE', ])
 # @app.route('/api/lab1/check', methods=['GET'])
 # def check_lab():
 #     pass
 
 
+with app.app_context():
+    db.create_all()
+
+
 if __name__ == '__main__':
+    api_creation('lab1')
     app.run(debug=True)
+    db.init_app(app)
