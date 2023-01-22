de3a6f19d5e1a743d007145ebad803daec7501c3
poli <polishka3004@gmail.com>
2023-01-22 16:50:57 +0300

    api: add user GET, POST, DELETE requests and code refactoring

--- api.py
+++ api.py
@@ -17,26 +17,16 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
 
 
-class Lab1(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, unique=True, nullable=False)
-    state = db.Column(db.String)
-
-
 def api_creation(lab_name):
-    class UserAPI(MethodView):
+    class Lab(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        user_id = db.Column(db.Integer, unique=True, nullable=False)
+        state = db.Column(db.String)
+
+    class LabAPI(MethodView):
         def __init__(self):
             self.dir = f'{dir}/{lab_name}'
             self.cli = TerraformCommand(self.dir)
-
-        # list users or lab status
-        def get(self, user_id):
-            if user_id is None:
-                stdout = self.cli.workspace('list').value
-                return stdout.split('\n')
-            else:
-                self.cli.workspace('select', str(user_id))
-                return self.cli.state('list').value
 
         # func for ferraform apply/destroy
         def change_lab(self, action, userid):
@@ -51,6 +41,15 @@ def api_creation(lab_name):
             db.session.commit()
             return user.id
 
+# list users or lab status
+        def get(self, user_id):
+            if user_id is None:
+                stdout = self.cli.workspace('list').value
+                return stdout.split('\n')
+            else:
+                self.cli.workspace('select', str(user_id))
+                return self.cli.state('list').value
+
         # deploy lab
         def post(self):
             data = request.get_json()
@@ -59,7 +58,7 @@ def api_creation(lab_name):
                 self.thread = Thread(target=self.change_lab,
                                      args=('apply', user_id), daemon=True)
                 self.thread.start()
-                return {'status': 202, 
+                return {'status': 202,
                         'message': f'{lab_name} is creating...'}, 202
             else:
                 return {'status': 409,
@@ -75,16 +74,60 @@ def api_creation(lab_name):
                 return {'status': 202,
                         'message': f'{lab_name} is destroying...'}, 202
             else:
-                return {'status': 409,
-                        'message': f'{lab_name} is not exists'}, 409
+                return {'status': 404,
+                        'message': f'{lab_name} not found'}, 404
 
-    user_view = UserAPI.as_view('user_api')
+    lab_view = LabAPI.as_view('lab_api')
     app.add_url_rule(f'/api/{lab_name}', defaults={'user_id': None},
-                     view_func=user_view, methods=['GET', ])
-    app.add_url_rule(f'/api/{lab_name}', view_func=user_view,
+                     view_func=lab_view, methods=['GET', ])
+    app.add_url_rule(f'/api/{lab_name}', view_func=lab_view,
                      methods=['POST', ])
-    app.add_url_rule(f'/api/{lab_name}/<int:user_id>', view_func=user_view,
+    app.add_url_rule(f'/api/{lab_name}/<int:user_id>', view_func=lab_view,
                      methods=['GET', 'DELETE', ])
+
+
+class UserAPI(MethodView):
+    def get(self, user_id):
+        if user_id is None:
+            users = User.query.all()
+            return {'users': [{'id': user.id,
+                               'username': user.username,
+                               'email': user.email} for user in users]}
+        else:
+            user = db.session.get(User, user_id)
+            if user is None:
+                return {'status': 404, 'message': 'User not found'}
+            else:
+                return {'user': {'id': user.id,
+                                 'username': user.username,
+                                 'email': user.email}}
+
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'], email=data['email'])
+        db.session.add(user)
+        db.session.commit()
+        return {'id': user.id, 'username': user.username, 'email': user.email}
+
+    def delete(self, user_id):
+        user = db.session.get(User, user_id)
+        if user is None:
+            return {'status': 404, 'message': 'User not found'}
+        else:
+            db.session.delete(user)
+            db.session.commit()
+            return {'status:': 200, 'message': 'User succesfully deleted'}
+
+
+user_view = UserAPI.as_view('user_api')
+app.add_url_rule('/api/user/', view_func=user_view, methods=['GET', ],
+                 defaults={'user_id': None})
+app.add_url_rule('/api/user/<int:user_id>', view_func=user_view,
+                 methods=['GET', ])
+app.add_url_rule('/api/user/', view_func=user_view, methods=['POST', ])
+app.add_url_rule('/api/user/<int:user_id>',
+                 view_func=user_view, methods=['DELETE', ])
+
 # @app.route('/api/lab1/check', methods=['GET'])
 # def check_lab():
 #     pass
