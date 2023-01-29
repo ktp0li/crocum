d25de15d06ff97a1cf35928b52a327eb53cc99d9
poli <polishka3004@gmail.com>
2023-01-29 18:51:38 +0300

    api: fix terraform apply

--- api.py
+++ api.py
@@ -4,11 +4,11 @@ from flask.views import MethodView
 from libterraform import TerraformCommand
 from threading import Thread
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = ""
 db = SQLAlchemy(app)
-dir = '/home/user/lab/terraform'
 
 
 class User(db.Model):
@@ -17,52 +17,66 @@ class User(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
 
 
-def api_creation(lab_name):
-    class Lab(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(db.Integer, unique=True, nullable=False)
-        state = db.Column(db.String)
+class Lab(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    state = db.Column(db.String)
 
+
+app.before_first_request(db.create_all)
+
+
+def api_creation(lab_name):
     class LabAPI(MethodView):
         def __init__(self):
-            self.dir = f'{dir}/{lab_name}'
+            self.dir = f'{os.getcwd()}/terraform/{lab_name}'
             self.cli = TerraformCommand(self.dir)
 
         # func for ferraform apply/destroy
-        def change_lab(self, action, userid):
-            self.cli.workspace('select', userid)
-            option = {'var': f'user-id={userid}', 'auto-approve': ...}
-            self.cli.run(action, option)
+        def change_lab(self, action, labid):
+            self.cli.workspace('add', str(labid))
+            self.cli.workspace('select', str(labid))
+            #lab = db.session.get(Lab, labid)
+            option = {'var': f'user-id={labid}', 'auto-approve': ...}
+            print(self.cli.run(cmd=action, options=option))
+            #db.session.add(lab)
+            #db.session.commit()
             self.cli.workspace('select', 'default')
 
-        def create_user(self, username, email):
-            user = User(username=username, email=email)
-            db.session.add(user)
-            db.session.commit()
-            return user.id
+        def create_lab_in_db(self, user_id):
+            if (db.session.get(User, user_id)) is not None:
+                lab = Lab(user_id=user_id, state='')
+                db.session.add(lab)
+                db.session.commit()
+                print(lab.id)
+                return lab.id
+            else:
+                return None
 
-# list users or lab status
+        # list users or lab status
         def get(self, user_id):
             if user_id is None:
-                stdout = self.cli.workspace('list').value
-                return stdout.split('\n')
+                labs = Lab.query.all()
+                return {'status': 200, 'message': {'labs':
+                                                   [{'id': lab.id,
+                                                     'user_id': lab.user_id,
+                                                     'state': lab.state}
+                                                    for lab in labs]}}
             else:
                 self.cli.workspace('select', str(user_id))
                 return self.cli.state('list').value
 
         # deploy lab
-        def post(self):
-            data = request.get_json()
-            user_id = str(self.create_user(data['username'], data['email']))
-            if self.cli.workspace('new', user_id).retcode in (0, 2):
-                self.thread = Thread(target=self.change_lab,
-                                     args=('apply', user_id), daemon=True)
+        def post(self, user_id):
+            if (lab_id := self.create_lab_in_db(user_id)) is not None:
+                self.thread = Thread(
+                      target=self.change_lab,
+                      args=('apply', lab_id), daemon=True)
                 self.thread.start()
                 return {'status': 202,
-                        'message': f'{lab_name} is creating...'}, 202
+                        'message': {'id': lab_id}}, 202
             else:
-                return {'status': 409,
-                        'message': f'{lab_name} already exist'}, 409
+                return {'status': 404, 'message': "User not found"}
 
         # destroy lab
         def delete(self, user_id):
@@ -80,10 +94,8 @@ def api_creation(lab_name):
     lab_view = LabAPI.as_view('lab_api')
     app.add_url_rule(f'/api/{lab_name}', defaults={'user_id': None},
                      view_func=lab_view, methods=['GET', ])
-    app.add_url_rule(f'/api/{lab_name}', view_func=lab_view,
-                     methods=['POST', ])
     app.add_url_rule(f'/api/{lab_name}/<int:user_id>', view_func=lab_view,
-                     methods=['GET', 'DELETE', ])
+                     methods=['GET', 'DELETE', 'POST', ])
 
 
 class UserAPI(MethodView):
@@ -151,11 +163,6 @@ app.add_url_rule('/api/user/<int:user_id>',
 #     pass
 
 
-with app.app_context():
-    db.create_all()
-
-
 if __name__ == '__main__':
     api_creation('lab1')
     app.run(debug=True)
-    db.init_app(app)
